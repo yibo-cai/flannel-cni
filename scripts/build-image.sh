0bbe926e38f623b2ca23913d0f3511d1a969843a
Yibo Cai <yibo.cai@linaro.org>
2017-11-09 17:54:10 +0800

    build arm64

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -e
 CURL="curl -sSf"
-FLANNEL_CNI_ROOT=$(git rev-parse --show-toplevel)
-IMAGE_NAME=quay.io/coreos/flannel-cni
-VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)
+IMAGE_NAME=linaro/flannel-cni-arm64
+VERSION=v0.2.0
 CNI_VERSION="v0.6.0"
 
 mkdir -p dist
-$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
-$CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
+$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-arm64-$CNI_VERSION.tgz | tar -xz -C dist/
+$CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-arm64-$CNI_VERSION.tgz | tar -xz -C dist/
 
 docker build --no-cache -t $IMAGE_NAME:$VERSION .
 docker push $IMAGE_NAME:$VERSION
